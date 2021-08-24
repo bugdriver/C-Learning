523dae2f513049b26a2753a556698c907ea5da0c
rajat <rajat4113@gmail.com>
2021-08-24 09:45:09 +0530

    modified index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const getDetails = function () {
   const dists = document.getElementsByClassName("dist");
-  const details = [...dists].map(function (dist) {
+  return [...dists].map(function (dist) {
     const name = dist.getElementsByTagName("select")[0].value;
     const address = dist.getElementsByTagName("textArea")[0].value;
     return { name: name, address: address };
   });
-  return details;
 };
 
 const main = function () {
